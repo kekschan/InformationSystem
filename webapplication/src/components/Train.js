61e34b4d31eb77f73434b1ab4e942c5f92719b33
Denis Borshev <108779927+kekschan@users.noreply.github.com>
2023-08-04 13:14:27 +0000

    надо закончить панель с кнопкой добавления

--- webapplication/src/components/Train.js
+++ webapplication/src/components/Train.js
@@ -39,63 +39,81 @@ export default class Train extends Component {
 
         return (
             <body  style={styles.cardContainer}>
-            {/*сделать по-середине*/}
-            <div >
-             {this.state.train.length === 0 ?
-                (<div className="fade-in-card" style={{width: '60rem'}}>
-                    <Card className="red-border">
-                        <Card.Body>
+                <div >
+                    <div style={{ display: 'inline-block', marginBlockEnd: '15px', fontSize: '24px', lineHeight: '1.5' }}>
+                        <Card className="custom-card">
+                            <Card.Header as="h4">
                             <div className="text-center">
-                                <Card.Title>На данный момент нет добавленных поездов</Card.Title>
+                                <div style={{marginBlockEnd: '5px'}}>Добро пожаловать на страницу системного администрирования.</div>
+                                <div>Здесь вы можете добавлять, удалять и изменять поезда на сервере. Удачной вам работы!</div>
                             </div>
-                        </Card.Body>
-                    </Card>
-                </div>) 
-                : (
-                <div className="fade-in-card">
-                    <Card className="red-border">
-                        <Card.Header as="h5" className="card-header-container">
-                            <div className="text-start">
-                                <div style={{display: 'inline-block', marginRight: '10px'}}>№444A</div>
-                                <div style={{display: 'inline-block'}}>Пассажирский</div>
-                            </div>
-                            <div className="text-end d-md-block d-none">
-                                <div style={{display: 'inline-block', marginRight: '10px'}}>Москва</div>
-                                <div style={{display: 'inline-block', marginRight: '10px'}}><FontAwesomeIcon icon={faLongArrowRight}></FontAwesomeIcon></div>
-                                <div style={{display: 'inline-block'}}>Лондон</div>
-                            </div>
-                        </Card.Header>
-                        <Card.Body style={{ height: '140px', display: 'flex', alignItems: 'center' }}>
-                            <Card.Text>
-                                <Container>
-                                <Row>
-                                    <Col xxl="9" xl="9" lg="9" md="9">
-                                    <div className="d-flex justify-content-center">
-                                        <img
-                                        src={passengerImage}
-                                        alt="Passenger"
-                                        className="img-fluid d-md-block d-none"
-                                        style={{ width: '740px', height: '65px' }}
-                                        />
-                                    </div>
-                                    </Col>
-                                    <Col>
-                                     Сделать вывод через map 
-                                    </Col>
-                                </Row>
-                                </Container>
-                            </Card.Text>
-                            </Card.Body>
-                        <Card.Footer className="card-footer-container">
-                            <div className="text-start"><Button variant="primary">Подробнее</Button></div>
-                            <div className="text-end"><Button variant="primary">Изменить</Button></div>
-                        </Card.Footer>
-                    </Card>
+                            </Card.Header>
+                            <Card.Footer className="text-end">
+                            <Button variant="success">Добавить</Button>
+                            </Card.Footer>
+                        </Card>
+                        </div>
+                    <div>
+                        {this.state.train.length === 0 ?
+                            (<div className="fade-in-card">
+                                <Card className="red-border">
+                                    <Card.Body>
+                                        <div className="text-center">
+                                            <Card.Title>На данный момент нет добавленных поездов</Card.Title>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </div>) 
+                            : (
+                            <div className="fade-in-card">
+                                <Card className="red-border">
+                                        <Card.Header as="h5" className="card-header-container">
+                                            <div className="text-start">
+                                                <div style={{display: 'inline-block', marginRight: '10px'}}>№444A</div>
+                                                <div style={{display: 'inline-block'}}>Пассажирский</div>
+                                            </div>
+                                            <div className="text-end d-md-block d-none">
+                                                <div style={{display: 'inline-block', marginRight: '10px'}}>Москва</div>
+                                                <div style={{display: 'inline-block', marginRight: '10px'}}><FontAwesomeIcon icon={faLongArrowRight}></FontAwesomeIcon></div>
+                                                <div style={{display: 'inline-block'}}>Лондон</div>
+                                            </div>
+                                        </Card.Header>
+                                        <Card.Body style={{ height: '140px', display: 'flex', alignItems: 'center' }}>
+                                            <Card.Text>
+                                                <Container>
+                                                    <Row>
+                                                        <Col xxl="9" xl="9" lg="9" md="9">
+                                                            <div className="d-flex justify-content-center">
+                                                                <img
+                                                                src={passengerImage}
+                                                                alt="Passenger"
+                                                                className="img-fluid d-md-block d-none"
+                                                                style={{ width: '740px', height: '65px' }}/>
+                                                            </div>
+                                                        </Col>
+                                                        <Col>
+                                                        Сделать вывод через map 
+                                                        </Col>
+                                                    </Row>
+                                                </Container>
+                                            </Card.Text>
+                                        </Card.Body>
+                                        <Card.Footer className="card-footer-container">
+                                            <div className="text-start">
+                                                <Button variant="outline-danger light"className="custom-btn">Подробнее</Button>
+                                            </div>
+                                            <div className="text-end">
+                                                <Button variant="danger">Удалить</Button>
+                                                <Button variant="info">Изменить</Button>
+                                            </div>
+                                        </Card.Footer>
+                                    </Card>
+                                </div>
+                             )
+                        }
+            
+                    </div>
                 </div>
-            )
-            }
-        
-            </div>
             </body>
         );
     }
